6a1d9f9096c5533de8b68ab06c61bee2613109db
Adetunjii <adetunjithomas1@outlook.com>
2020-04-08 01:38:28 +0100

    correction 6

--- src/about.jsx
+++ src/about.jsx
@@ -5,7 +5,7 @@ import * as quality from "./assets/quality.png";
 import * as specialist from "./assets/download.png";
 import * as pricing from "./assets/pricing.png";
 
-const About = props => {
+const About = (props) => {
   return (
     <MDBContainer id={props.id} className="bg-transparent margin-extreme">
       <div className="row py-5">
@@ -26,7 +26,7 @@ const About = props => {
           <p className="about-text">
             international clients. Our daily goal is to enhance the perception
             of doing business and sourcing food ingredients in Africa and to
-            delivery consistent quality in a timely fashio.
+            delivery consistent quality in a timely fashion.
           </p>
         </div>
       </div>
@@ -41,7 +41,7 @@ const About = props => {
                 alt="quality"
               />
               <h3 className="py-2 quality-header h2-responsive">Quality</h3>
-              <p className="quality-text text-center">
+              <p className="quality-text text-left">
                 We aim to trade only the highest quality products and are
                 continuously striving to improve our standards to drive
                 international competitiveness
@@ -50,7 +50,7 @@ const About = props => {
             <div className="col-md-4 p-5">
               <img className="quality-img" src={specialist} alt="specialist" />
               <h2 className="quality-header py-2 h2-responsive">Specialists</h2>
-              <p className="quality-text text-center">
+              <p className="quality-text text-left">
                 We are specialist, global traders in niche commodities, their
                 markets, and seasons.
               </p>
@@ -62,7 +62,7 @@ const About = props => {
                 alt="pricing"
               />
               <h2 className="quality-header h2-responsive">Pricing</h2>
-              <p className="quality-text">
+              <p className="quality-text text-left">
                 We aim to source quality commodities at highly competitive, but
                 fair prices in order to pass maximum value to our customers and
                 trading partners
